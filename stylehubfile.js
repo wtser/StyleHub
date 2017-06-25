56d1656c071381c0798538e09f503ae5a6248a09
wtser <wtser@qq.com>
2017-06-25 13:50:48 +0800

    Add miaopai

--- stylehubfile.js
+++ stylehubfile.js
@@ -6,5 +6,11 @@ let stylesheet = [
     {
         pattern: /weibo.com\/tv\/v\/[\w]+/,
         styleText: `.weibo_player_fc_v2,.player_switch{display:none!important;}.weibo_player_fb,.weibo_player_fa{width:100%!important;}.weibo_player_fa{width:100%;height: 0!important;padding-bottom: 76%;}`,
+    },
+    {
+        pattern: /www.miaopai.com\/show\/[\w]+/,
+        styleText: `.cotentRight{display:none!important;}.box885{width: 990px!important;}.video_detail .video{height:100%!important;}
+        .contentLeft_detail,.MIAOPAI_player{width:100%!important;}.MIAOPAI_player .video-player{height: 0!important;
+    padding-bottom: 76%;}.video_detail .video{    position: absolute;}.contentLeft,.videoIntr+.video{    position: relative;}`,
     }
 ]
