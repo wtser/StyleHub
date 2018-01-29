df9631afed42552c744b316c3cc8421864251407
wtser <wtser@qq.com>
2018-01-29 21:25:17 +0800

    Add ignore

--- script/content.js
+++ script/content.js
@@ -9,44 +9,48 @@
 //     })
 // };
 
-let getSyncFile = function () {
-    return new Promise(function (reslove, reject) {
-        chrome.storage.sync.get('stylehubfile', function (content) {
-            reslove(content.stylehubfile);
-        })
-    })
+let getSyncFile = function() {
+  return new Promise(function(reslove, reject) {
+    chrome.storage.sync.get("stylehubfile", function(content) {
+      reslove(content.stylehubfile);
+    });
+  });
 };
 
-getSyncFile().then(function (syncStyle) {
-    if (syncStyle) {
-        //console.log(syncStyle)
+getSyncFile().then(function(syncStyle) {
+  if (syncStyle) {
+    //console.log(syncStyle)
+  } else {
+    chrome.storage.sync.set({ stylehubfile: initStyle });
+  }
+  let usingStyle = syncStyle || initStyle;
 
-    } else {
-        chrome.storage.sync.set({stylehubfile: initStyle});
+  let styleApply = function(styleText) {
+    let $style = document.createElement("style");
+    $style.type = "text/css";
+    $style.setAttribute("source", "stylehub");
+
+    $style.appendChild(document.createTextNode(styleText));
+    //$style.styleSheet.cssText = styleText;
+    document.head.appendChild($style);
+  };
+
+  let ignores = usingStyle.filter(us => us.pattern === "ignore");
+  let ignore = ignores.length != 0 ? ignores[0].styleText.split("\n") : [];
+
+  usingStyle.forEach(function(styleItem) {
+    let shoudIgnore = ignore.filter(igpattern => {
+      return new RegExp(igpattern).test(location.href);
+    });
+    if (shoudIgnore.length != 0) {
+      return false;
     }
-    let usingStyle = syncStyle || initStyle;
-
-    let styleApply = function (styleText) {
-        let $style = document.createElement('style');
-        $style.type = 'text/css';
-        $style.setAttribute('source', 'stylehub');
-
-        $style.appendChild(document.createTextNode(styleText));
-        //$style.styleSheet.cssText = styleText;
-        document.head.appendChild($style)
-
-    };
-
-    usingStyle.forEach(function (styleItem) {
-        let isUrlMatch = (new RegExp(styleItem.pattern)).test(location.href);
-        if (isUrlMatch) {
-            styleApply(styleItem.styleText);
-            if (styleItem.scriptText) {
-                eval(styleItem.scriptText)
-            }
-        }
-    })
+    let isUrlMatch = new RegExp(styleItem.pattern).test(location.href);
+    if (isUrlMatch) {
+      styleApply(styleItem.styleText);
+      if (styleItem.scriptText) {
+        eval(styleItem.scriptText);
+      }
+    }
+  });
 });
-
-
-
